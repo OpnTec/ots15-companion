bf7b19ff5be11cb5317777b4c408156b1177b128
mananwason <manan13056@iiitd.ac.in>
2015-05-05 22:00:59 +0530

    date changed from march to may

--- app/src/main/java/org/opentech/fragments/ScheduleFragment.java
+++ app/src/main/java/org/opentech/fragments/ScheduleFragment.java
@@ -49,9 +49,9 @@ public class ScheduleFragment extends Fragment {
 
 
             ArrayList<Day> staticDays = new ArrayList<>();
-            staticDays.add(new Day(0, "March 13"));
-            staticDays.add(new Day(1, "March 14"));
-            staticDays.add(new Day(2, "March 15"));
+            staticDays.add(new Day(0, "May 13"));
+            staticDays.add(new Day(1, "May 14"));
+            staticDays.add(new Day(2, "May 15"));
 
 
             daysAdapter = new DayLoader(getChildFragmentManager(), track, staticDays);
